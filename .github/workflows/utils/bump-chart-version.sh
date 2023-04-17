004460882a85a07c20a59070bf6b535d6e88969d
Miguel Martinez Trivino <miguel@chainloop.dev>
2023-04-17 16:32:15 +0200

    chore: fix helm chart versioning (#76)
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- .github/workflows/utils/bump-chart-version.sh
+++ .github/workflows/utils/bump-chart-version.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Bump Helm Chart version, and appVersion to a given version number
+# Bump Helm Chart version, appVersion to a given version number
 
 set -e
 
 die () {
     echo >&2 "$@"
-    echo "usage: bump.sh [chartYamlFile] [version]"
+    echo "usage: bump.sh [chartPath] [version]"
     exit 1
 }
 
@@ -17,9 +17,17 @@ fi
 
 [ "$#" -eq 2 ] || die "2 arguments required, $# provided"
 
-chart_yaml="${1}"
-version="${2}"
+chart_yaml="${1}/Chart.yaml"
+
+# Remove v prefix if provided
+semVer="$(echo ${2} | sed -e 's/^v\(.*\)/\1/')"
 
 
-sed -i "s#^appVersion:.*#appVersion: ${version}#g" "${chart_yaml}"
-sed -i "s#^version:.*#version: ${version}#g" "${chart_yaml}"
+# AppVersion includes a v prefix
+sed -i "s#^appVersion:.*#appVersion: v${semVer}#g" "${chart_yaml}"
+
+# Bump chart version MINOR and reset PATCH segment
+# A new release means a bump in the minor segment of the Chart and a reset of the patch one
+# i.e 1.0.2 => 1.1.0
+chartVersion=$(cat ${chart_yaml} | awk -F'[ .]' '/^version:/ {print $2"."$3+1"."0}')
+sed -i "s#^version:.*#version: ${chartVersion}#g" "${chart_yaml}"
