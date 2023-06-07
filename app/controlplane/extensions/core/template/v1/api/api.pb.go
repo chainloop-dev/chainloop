0486f52d8123337edb2ef61591cd24ae653f11ef
Miguel Martinez Trivino <miguel@chainloop.dev>
2023-06-07 15:34:39 +0200

    doc(integrations): create template
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- app/controlplane/extensions/core/template/v1/api/api.pb.go
+++ app/controlplane/extensions/core/template/v1/api/api.pb.go
@@ -17,7 +17,7 @@
 // versions:
 // 	protoc-gen-go v1.30.0
 // 	protoc        (unknown)
-// source: template/v1/api/api.proto
+// source: core/template/v1/api/api.proto
 
 package api
 
@@ -48,7 +48,7 @@ type RegistrationRequest struct {
 func (x *RegistrationRequest) Reset() {
 	*x = RegistrationRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_template_v1_api_api_proto_msgTypes[0]
+		mi := &file_core_template_v1_api_api_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -61,7 +61,7 @@ func (x *RegistrationRequest) String() string {
 func (*RegistrationRequest) ProtoMessage() {}
 
 func (x *RegistrationRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_template_v1_api_api_proto_msgTypes[0]
+	mi := &file_core_template_v1_api_api_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -74,7 +74,7 @@ func (x *RegistrationRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use RegistrationRequest.ProtoReflect.Descriptor instead.
 func (*RegistrationRequest) Descriptor() ([]byte, []int) {
-	return file_template_v1_api_api_proto_rawDescGZIP(), []int{0}
+	return file_core_template_v1_api_api_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *RegistrationRequest) GetTest() string {
@@ -94,7 +94,7 @@ type AttachmentRequest struct {
 func (x *AttachmentRequest) Reset() {
 	*x = AttachmentRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_template_v1_api_api_proto_msgTypes[1]
+		mi := &file_core_template_v1_api_api_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -107,7 +107,7 @@ func (x *AttachmentRequest) String() string {
 func (*AttachmentRequest) ProtoMessage() {}
 
 func (x *AttachmentRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_template_v1_api_api_proto_msgTypes[1]
+	mi := &file_core_template_v1_api_api_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -120,47 +120,48 @@ func (x *AttachmentRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use AttachmentRequest.ProtoReflect.Descriptor instead.
 func (*AttachmentRequest) Descriptor() ([]byte, []int) {
-	return file_template_v1_api_api_proto_rawDescGZIP(), []int{1}
+	return file_core_template_v1_api_api_proto_rawDescGZIP(), []int{1}
 }
 
-var File_template_v1_api_api_proto protoreflect.FileDescriptor
+var File_core_template_v1_api_api_proto protoreflect.FileDescriptor
 
-var file_template_v1_api_api_proto_rawDesc = []byte{
-	0x0a, 0x19, 0x74, 0x65, 0x6d, 0x70, 0x6c, 0x61, 0x74, 0x65, 0x2f, 0x76, 0x31, 0x2f, 0x61, 0x70,
-	0x69, 0x2f, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x16, 0x65, 0x78, 0x74,
-	0x65, 0x6e, 0x73, 0x69, 0x6f, 0x6e, 0x73, 0x2e, 0x74, 0x65, 0x6d, 0x70, 0x6c, 0x61, 0x74, 0x65,
-	0x2e, 0x76, 0x31, 0x1a, 0x17, 0x76, 0x61, 0x6c, 0x69, 0x64, 0x61, 0x74, 0x65, 0x2f, 0x76, 0x61,
-	0x6c, 0x69, 0x64, 0x61, 0x74, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x32, 0x0a, 0x13,
-	0x52, 0x65, 0x67, 0x69, 0x73, 0x74, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x12, 0x1b, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x09, 0x42, 0x07, 0xfa, 0x42, 0x04, 0x72, 0x02, 0x10, 0x01, 0x52, 0x04, 0x74, 0x65, 0x73, 0x74,
-	0x22, 0x13, 0x0a, 0x11, 0x41, 0x74, 0x74, 0x61, 0x63, 0x68, 0x6d, 0x65, 0x6e, 0x74, 0x52, 0x65,
-	0x71, 0x75, 0x65, 0x73, 0x74, 0x42, 0x50, 0x5a, 0x4e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x6c, 0x6f, 0x6f, 0x70, 0x2d, 0x64, 0x65,
-	0x76, 0x2f, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x6c, 0x6f, 0x6f, 0x70, 0x2f, 0x61, 0x70, 0x70, 0x2f,
-	0x63, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x70, 0x6c, 0x61, 0x6e, 0x65, 0x2f, 0x65, 0x78, 0x74,
-	0x65, 0x6e, 0x73, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x74, 0x65, 0x6d, 0x70, 0x6c, 0x61, 0x74, 0x65,
-	0x2f, 0x76, 0x31, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_core_template_v1_api_api_proto_rawDesc = []byte{
+	0x0a, 0x1e, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x74, 0x65, 0x6d, 0x70, 0x6c, 0x61, 0x74, 0x65, 0x2f,
+	0x76, 0x31, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x12, 0x16, 0x65, 0x78, 0x74, 0x65, 0x6e, 0x73, 0x69, 0x6f, 0x6e, 0x73, 0x2e, 0x74, 0x65, 0x6d,
+	0x70, 0x6c, 0x61, 0x74, 0x65, 0x2e, 0x76, 0x31, 0x1a, 0x17, 0x76, 0x61, 0x6c, 0x69, 0x64, 0x61,
+	0x74, 0x65, 0x2f, 0x76, 0x61, 0x6c, 0x69, 0x64, 0x61, 0x74, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x22, 0x32, 0x0a, 0x13, 0x52, 0x65, 0x67, 0x69, 0x73, 0x74, 0x72, 0x61, 0x74, 0x69, 0x6f,
+	0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1b, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x42, 0x07, 0xfa, 0x42, 0x04, 0x72, 0x02, 0x10, 0x01, 0x52,
+	0x04, 0x74, 0x65, 0x73, 0x74, 0x22, 0x13, 0x0a, 0x11, 0x41, 0x74, 0x74, 0x61, 0x63, 0x68, 0x6d,
+	0x65, 0x6e, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x42, 0x50, 0x5a, 0x4e, 0x67, 0x69,
+	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x6c, 0x6f,
+	0x6f, 0x70, 0x2d, 0x64, 0x65, 0x76, 0x2f, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x6c, 0x6f, 0x6f, 0x70,
+	0x2f, 0x61, 0x70, 0x70, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x70, 0x6c, 0x61, 0x6e,
+	0x65, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x6e, 0x73, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x74, 0x65, 0x6d,
+	0x70, 0x6c, 0x61, 0x74, 0x65, 0x2f, 0x76, 0x31, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_template_v1_api_api_proto_rawDescOnce sync.Once
-	file_template_v1_api_api_proto_rawDescData = file_template_v1_api_api_proto_rawDesc
+	file_core_template_v1_api_api_proto_rawDescOnce sync.Once
+	file_core_template_v1_api_api_proto_rawDescData = file_core_template_v1_api_api_proto_rawDesc
 )
 
-func file_template_v1_api_api_proto_rawDescGZIP() []byte {
-	file_template_v1_api_api_proto_rawDescOnce.Do(func() {
-		file_template_v1_api_api_proto_rawDescData = protoimpl.X.CompressGZIP(file_template_v1_api_api_proto_rawDescData)
+func file_core_template_v1_api_api_proto_rawDescGZIP() []byte {
+	file_core_template_v1_api_api_proto_rawDescOnce.Do(func() {
+		file_core_template_v1_api_api_proto_rawDescData = protoimpl.X.CompressGZIP(file_core_template_v1_api_api_proto_rawDescData)
 	})
-	return file_template_v1_api_api_proto_rawDescData
+	return file_core_template_v1_api_api_proto_rawDescData
 }
 
-var file_template_v1_api_api_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_template_v1_api_api_proto_goTypes = []interface{}{
+var file_core_template_v1_api_api_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_core_template_v1_api_api_proto_goTypes = []interface{}{
 	(*RegistrationRequest)(nil), // 0: extensions.template.v1.RegistrationRequest
 	(*AttachmentRequest)(nil),   // 1: extensions.template.v1.AttachmentRequest
 }
-var file_template_v1_api_api_proto_depIdxs = []int32{
+var file_core_template_v1_api_api_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -168,13 +169,13 @@ var file_template_v1_api_api_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_template_v1_api_api_proto_init() }
-func file_template_v1_api_api_proto_init() {
-	if File_template_v1_api_api_proto != nil {
+func init() { file_core_template_v1_api_api_proto_init() }
+func file_core_template_v1_api_api_proto_init() {
+	if File_core_template_v1_api_api_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_template_v1_api_api_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_core_template_v1_api_api_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*RegistrationRequest); i {
 			case 0:
 				return &v.state
@@ -186,7 +187,7 @@ func file_template_v1_api_api_proto_init() {
 				return nil
 			}
 		}
-		file_template_v1_api_api_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_core_template_v1_api_api_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttachmentRequest); i {
 			case 0:
 				return &v.state
@@ -203,18 +204,18 @@ func file_template_v1_api_api_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_template_v1_api_api_proto_rawDesc,
+			RawDescriptor: file_core_template_v1_api_api_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_template_v1_api_api_proto_goTypes,
-		DependencyIndexes: file_template_v1_api_api_proto_depIdxs,
-		MessageInfos:      file_template_v1_api_api_proto_msgTypes,
+		GoTypes:           file_core_template_v1_api_api_proto_goTypes,
+		DependencyIndexes: file_core_template_v1_api_api_proto_depIdxs,
+		MessageInfos:      file_core_template_v1_api_api_proto_msgTypes,
 	}.Build()
-	File_template_v1_api_api_proto = out.File
-	file_template_v1_api_api_proto_rawDesc = nil
-	file_template_v1_api_api_proto_goTypes = nil
-	file_template_v1_api_api_proto_depIdxs = nil
+	File_core_template_v1_api_api_proto = out.File
+	file_core_template_v1_api_api_proto_rawDesc = nil
+	file_core_template_v1_api_api_proto_goTypes = nil
+	file_core_template_v1_api_api_proto_depIdxs = nil
 }
