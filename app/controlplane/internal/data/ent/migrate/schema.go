3914fcfa96dc8f0fd7a29152a6565bef70b977cb
Miguel Martinez Trivino <miguel@chainloop.dev>
2023-09-26 23:25:32 +0200

    implement downloader/uploader
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- app/controlplane/internal/data/ent/migrate/schema.go
+++ app/controlplane/internal/data/ent/migrate/schema.go
@@ -12,7 +12,7 @@ var (
 	CasBackendsColumns = []*schema.Column{
 		{Name: "id", Type: field.TypeUUID, Unique: true},
 		{Name: "location", Type: field.TypeString},
-		{Name: "provider", Type: field.TypeEnum, Enums: []string{"OCI", "AzureBlob", "INLINE"}},
+		{Name: "provider", Type: field.TypeEnum, Enums: []string{"AzureBlob", "OCI", "INLINE"}},
 		{Name: "description", Type: field.TypeString, Nullable: true},
 		{Name: "secret_name", Type: field.TypeString},
 		{Name: "created_at", Type: field.TypeTime, Default: "CURRENT_TIMESTAMP"},
