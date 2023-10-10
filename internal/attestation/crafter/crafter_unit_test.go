a6e658adce6302859a3db6001c7ef1f960a07d36
Miguel Martinez Trivino <miguel@chainloop.dev>
2023-10-10 17:39:29 +0200

    chore: fix tests
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- internal/attestation/crafter/crafter_unit_test.go
+++ internal/attestation/crafter/crafter_unit_test.go
@@ -16,10 +16,14 @@
 package crafter
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/suite"
 )
@@ -29,34 +33,88 @@ type crafterUnitSuite struct {
 }
 
 func (s *crafterUnitSuite) TestGitRepoHead() {
+	initRepo := func(withCommit bool) func(string) (string, error) {
+		return func(repoPath string) (string, error) {
+			repo, err := git.PlainInit(repoPath, false)
+			if err != nil {
+				return "", err
+			}
+
+			if withCommit {
+				wt, err := repo.Worktree()
+				if err != nil {
+					return "", err
+				}
+
+				filename := filepath.Join(repoPath, "example-git-file")
+				if err = os.WriteFile(filename, []byte("hello world!"), 0600); err != nil {
+					return "", err
+				}
+
+				_, err = wt.Add("example-git-file")
+				if err != nil {
+					return "", err
+				}
+
+				h, err := wt.Commit("test commit", &git.CommitOptions{})
+				if err != nil {
+					return "", err
+				}
+
+				fmt.Println("BOOOM", h)
+
+				return h.String(), nil
+			}
+
+			return "", nil
+		}
+	}
+
 	testCases := []struct {
-		name     string
-		repoPath string
-		hash     string
-		wantErr  bool
+		name          string
+		repoProvider  func(string) (string, error)
+		wantErr       bool
+		wantEmptyHash bool
 	}{
 		{
-			name:     "happy path",
-			repoPath: "testdata/gitresolver/valid",
-			hash:     "5765a751153f5fa9c5d59fc201a802fa4096f081",
+			name:         "happy path",
+			repoProvider: initRepo(true),
 		},
 		{
-			name:     "not a repository",
-			repoPath: os.TempDir(),
-			hash:     "",
+			name:          "empty repo",
+			repoProvider:  initRepo(false),
+			wantEmptyHash: true,
+		},
+		{
+			name:          "not a repository",
+			wantEmptyHash: true,
 		},
 	}
 
 	for _, tc := range testCases {
 		s.Run(tc.name, func() {
-			got, err := gracefulGitRepoHead(tc.repoPath)
+			path := s.T().TempDir()
+			var wantDigest string
+			if tc.repoProvider != nil {
+				var err error
+				wantDigest, err = tc.repoProvider(path)
+				require.NoError(s.T(), err)
+			}
+
+			got, err := gracefulGitRepoHead(path)
 			if tc.wantErr {
 				assert.Error(s.T(), err)
 				return
 			}
 
+			if tc.wantEmptyHash {
+				assert.Empty(s.T(), got)
+			} else {
+				assert.NotEmpty(s.T(), got)
+			}
+
 			assert.NoError(s.T(), err)
-			assert.Equal(s.T(), tc.hash, got)
+			assert.Equal(s.T(), wantDigest, got)
 		})
 	}
 }
