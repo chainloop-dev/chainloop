b0e862ea294f716aeac14b6ef4d6276300af405e
Javier Rodriguez <javi.role1@gmail.com>
2023-11-08 15:48:50 +0100

    fix(attestation-api): Clean empty descriptions
    Signed-off-by: Javier Rodriguez <javi.role1@gmail.com>
    Signed-off-by: Javier Rodriguez <javier.rodleo@gmail.com>

--- app/controlplane/api/controlplane/v1/response_messages.pb.validate.go
+++ app/controlplane/api/controlplane/v1/response_messages.pb.validate.go
@@ -458,37 +458,6 @@ func (m *AttestationItem) validate(all bool) error {
 
 	var errors []error
 
-	// no validation rules for Id
-
-	if all {
-		switch v := interface{}(m.GetCreatedAt()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, AttestationItemValidationError{
-					field:  "CreatedAt",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		case interface{ Validate() error }:
-			if err := v.Validate(); err != nil {
-				errors = append(errors, AttestationItemValidationError{
-					field:  "CreatedAt",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		}
-	} else if v, ok := interface{}(m.GetCreatedAt()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return AttestationItemValidationError{
-				field:  "CreatedAt",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
-	}
-
 	// no validation rules for Envelope
 
 	// no validation rules for DigestInCasBackend
