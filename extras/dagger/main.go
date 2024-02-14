fcc391fbeea0b6a30ed265c63b7a0b5751b6de50
Miguel Martinez Trivino <miguel@chainloop.dev>
2024-02-14 13:25:09 +0100

    chore: run linter on dagger module
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- extras/dagger/main.go
+++ extras/dagger/main.go
@@ -1,17 +1,3 @@
-// Copyright 2024 The Chainloop Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -23,6 +9,7 @@ import (
 
 const (
 	// https://github.com/chainloop-dev/chainloop/releases/tag/v0.60.0
+	//nolint:gosec
 	clImage = "ghcr.io/chainloop-dev/chainloop/cli@sha256:4e0bc402f71f4877a1ae8d6df5eb4e666a0efa0e7d43ab4f97f21c0e46ae0a59"
 )
 
