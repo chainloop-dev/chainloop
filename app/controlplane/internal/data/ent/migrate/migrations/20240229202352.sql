2415936aa784456160b1d0b0b9ec8216211faa06
Miguel Martinez Trivino <miguel@chainloop.dev>
2024-03-01 17:43:55 +0100

    chore: fix migration
    Signed-off-by: Miguel Martinez Trivino <miguel@chainloop.dev>

--- app/controlplane/internal/data/ent/migrate/migrations/20240229202352.sql
+++ app/controlplane/internal/data/ent/migrate/migrations/20240229202352.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "memberships" ADD COLUMN "role" character varying;
 
 -- Set the existing memberships to the role "role:admin" to not to break compatibility
-UPDATE "memberships" SET "role" = 'role:admin';
+UPDATE "memberships" SET "role" = 'role:org:admin';
 
 -- enable the not null constraint
 ALTER TABLE "memberships" ALTER COLUMN "role" SET NOT NULL;
