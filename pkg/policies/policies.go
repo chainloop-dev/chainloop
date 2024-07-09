fbb5d11c43ff26a83e5f51f2ab10f36fe833e694
Jose I. Paris <jiparis@chainloop.dev>
2024-07-09 17:54:02 +0200

    inject cas client WIP
    Signed-off-by: Jose I. Paris <jiparis@chainloop.dev>

--- pkg/policies/policies.go
+++ pkg/policies/policies.go
@@ -21,6 +21,7 @@ import (
 
 	v1 "github.com/chainloop-dev/chainloop/app/controlplane/api/workflowcontract/v1"
 	v12 "github.com/chainloop-dev/chainloop/internal/attestation/crafter/api/attestation/v1"
+	"github.com/chainloop-dev/chainloop/internal/casclient"
 	"github.com/chainloop-dev/chainloop/pkg/policies/engine"
 	"github.com/chainloop-dev/chainloop/pkg/policies/engine/rego"
 	"github.com/sigstore/cosign/v2/pkg/blob"
@@ -29,13 +30,13 @@ import (
 )
 
 type PolicyVerifier struct {
-	state  *v12.CraftingState
-	engine engine.PolicyEngine
+	state *v12.CraftingState
+	cas   *casclient.Client
 }
 
-func NewPolicyVerifier(state *v12.CraftingState) *PolicyVerifier {
+func NewPolicyVerifier(state *v12.CraftingState, client *casclient.Client) *PolicyVerifier {
 	// only Rego engine is currently supported
-	return &PolicyVerifier{state: state, engine: new(rego.Rego)}
+	return &PolicyVerifier{state: state, cas: client}
 }
 
 // Verify verifies that the statement is compliant with the policies present in the schema
@@ -48,21 +49,21 @@ func (pv *PolicyVerifier) Verify(ctx context.Context) ([]*engine.PolicyViolation
 			// TODO: WARN.
 			continue
 		}
-		spec, err := loadSpec(policyAtt)
+		spec, err := pv.loadSpec(policyAtt)
 		if err != nil {
 			return nil, fmt.Errorf("failed to load policy spec: %w", err)
 		}
-		script, err := loadPolicyScriptFromSpec(spec)
+		script, err := pv.loadPolicyScriptFromSpec(spec)
 		if err != nil {
 			return nil, fmt.Errorf("failed to load policy content: %w", err)
 		}
-		m, err := loadSubject(policyAtt, spec, pv.state)
+		material, err := pv.loadSubject(policyAtt, spec, pv.state)
 		if err != nil {
 			return nil, fmt.Errorf("failed to load policy subject: %w", err)
 		}
 		// verify policy, passing arguments from policyAtt
-
-		res, err := pv.engine.Verify(ctx, script, m)
+		ng := getPolicyEngine(spec)
+		res, err := ng.Verify(ctx, script, material)
 		if err != nil {
 			return nil, fmt.Errorf("failed to verify policy: %w", err)
 		}
@@ -72,7 +73,7 @@ func (pv *PolicyVerifier) Verify(ctx context.Context) ([]*engine.PolicyViolation
 	return violations, nil
 }
 
-func loadSpec(attachment *v1.PolicyAttachment) (*v1.Policy, error) {
+func (pv *PolicyVerifier) loadSpec(attachment *v1.PolicyAttachment) (*v1.Policy, error) {
 	// 1. look for the referenced policy spec (note: `name` is not supported yet)
 	reference := attachment.GetRef()
 	// this method understands env, http and https schemes, and defaults to file system.
@@ -88,7 +89,7 @@ func loadSpec(attachment *v1.PolicyAttachment) (*v1.Policy, error) {
 }
 
 // loads a policy referenced from the spec
-func loadPolicyScriptFromSpec(spec *v1.Policy) (*engine.Policy, error) {
+func (pv *PolicyVerifier) loadPolicyScriptFromSpec(spec *v1.Policy) (*engine.Policy, error) {
 	var content []byte
 	var err error
 	if spec.GetSpec().GetEmbedded() != "" {
@@ -104,11 +105,12 @@ func loadPolicyScriptFromSpec(spec *v1.Policy) (*engine.Policy, error) {
 
 	return &engine.Policy{
 		Name:   spec.GetMetadata().GetName(),
-		Module: content,
+		Source: content,
 	}, nil
 }
 
-func loadSubject(attachment *v1.PolicyAttachment, spec *v1.Policy, state *v12.CraftingState) ([]byte, error) {
+// load the subject of the policy.
+func (pv *PolicyVerifier) loadSubject(attachment *v1.PolicyAttachment, spec *v1.Policy, state *v12.CraftingState) ([]byte, error) {
 	// Load the affected material or attestation, and checks if the expected name and type match
 	name := attachment.GetSelector().GetName()
 	// if name selector is not set, the subject will become the full crafting state
@@ -116,10 +118,14 @@ func loadSubject(attachment *v1.PolicyAttachment, spec *v1.Policy, state *v12.Cr
 		return protojson.Marshal(state.GetAttestation())
 	}
 
-	// if set, we want a material
+	// if name is set, we want a specific material
 	for _, m := range state.GetAttestation().GetMaterials() {
 		if m.GetArtifact().GetName() == name {
-			return getMaterialPayload(m)
+			if spec.GetSpec().GetKind() != v1.CraftingSchema_Material_MATERIAL_TYPE_UNSPECIFIED && spec.GetSpec().GetKind() != m.GetMaterialType() {
+				// If policy wasn't meant to be evaluated against this type of material, raise an error
+				return nil, fmt.Errorf("invalid material type: %s, policy spected: %s", m.GetMaterialType(), spec.GetSpec().GetKind())
+			}
+			return pv.getMaterialPayload(m)
 		}
 	}
 
@@ -127,13 +133,20 @@ func loadSubject(attachment *v1.PolicyAttachment, spec *v1.Policy, state *v12.Cr
 }
 
 // Gets the material payload from the CAS
-func getMaterialPayload(m *v12.Attestation_Material) ([]byte, error) {
-	// TODO
+func (pv *PolicyVerifier) getMaterialPayload(m *v12.Attestation_Material) ([]byte, error) {
+	if !m.UploadedToCas {
+		return m.GetArtifact().GetContent(), nil
+	}
+	// Look for material, and get its payload depending on which its nature.
+	// switch m.MaterialType {
+	// case v1.CraftingSchema_Mate
+	// }
+
 	return nil, nil
 }
 
-// GetPolicyEngine returns a PolicyEngine implementation to evaluate a given policy.
-func GetPolicyEngine(_ *v1.Policy) engine.PolicyEngine {
+// getPolicyEngine returns a PolicyEngine implementation to evaluate a given policy.
+func getPolicyEngine(_ *v1.Policy) engine.PolicyEngine {
 	// Currently, only Rego is supported
 	return new(rego.Rego)
 }
