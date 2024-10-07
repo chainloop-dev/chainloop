222dd18919861d548a57124bdb60b90457bc678a
Javier Rodriguez <javier@chainloop.dev>
2024-10-07 15:47:42 +0200

    feat(materials): Add support for TwistCTL scan reports
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

---
+++ pkg/attestation/crafter/materials/twistctl_scan.go
@@ -0,0 +1,70 @@
+//
+// Copyright 2024 The Chainloop Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package materials
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	schemaapi "github.com/chainloop-dev/chainloop/app/controlplane/api/workflowcontract/v1"
+	"github.com/chainloop-dev/chainloop/internal/casclient"
+	api "github.com/chainloop-dev/chainloop/pkg/attestation/crafter/api/attestation/v1"
+
+	"github.com/rs/zerolog"
+)
+
+type twistCTLScanResult struct {
+	Results    any    `json:"results"`
+	ConsoleURL string `json:"consoleURL"`
+}
+
+type TwistCTLScanCrafter struct {
+	backend *casclient.CASBackend
+	*crafterCommon
+}
+
+func NewTwistCTLScanCrafter(materialSchema *schemaapi.CraftingSchema_Material, backend *casclient.CASBackend, l *zerolog.Logger) (*TwistCTLScanCrafter, error) {
+	if materialSchema.Type != schemaapi.CraftingSchema_Material_TWISTCTL_SCAN_JSON {
+		return nil, fmt.Errorf("material type is not a twistcli scan")
+	}
+
+	return &TwistCTLScanCrafter{
+		backend:       backend,
+		crafterCommon: &crafterCommon{logger: l, input: materialSchema},
+	}, nil
+}
+
+func (i *TwistCTLScanCrafter) Craft(ctx context.Context, filePath string) (*api.Attestation_Material, error) {
+	f, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("can't open the file: %w", err)
+	}
+
+	var v twistCTLScanResult
+	if err := json.Unmarshal(f, &v); err != nil {
+		i.logger.Debug().Err(err).Msg("error decoding file")
+		return nil, fmt.Errorf("invalid twistctl scan file: %w", ErrInvalidMaterialType)
+	}
+
+	// Check the unmarshalled JSON contains a results and consoleURL fields
+	if v.Results == nil || v.ConsoleURL == "" {
+		return nil, fmt.Errorf("invalid twistctl scan file: %w", ErrInvalidMaterialType)
+	}
+
+	return uploadAndCraft(ctx, i.input, i.backend, filePath, i.logger)
+}
