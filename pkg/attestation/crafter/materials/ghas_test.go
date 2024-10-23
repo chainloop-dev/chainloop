0808e130ffaa6dec439beac5b5884cc698bf6c74
Jose I. Paris <jiparis@chainloop.dev>
2024-10-23 15:42:29 +0200

    apply suggestion and add test
    Signed-off-by: Jose I. Paris <jiparis@chainloop.dev>

---
+++ pkg/attestation/crafter/materials/ghas_test.go
@@ -0,0 +1,135 @@
+//
+// Copyright 2024 The Chainloop Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package materials_test
+
+import (
+	"context"
+	"testing"
+
+	contractAPI "github.com/chainloop-dev/chainloop/app/controlplane/api/workflowcontract/v1"
+	"github.com/chainloop-dev/chainloop/internal/casclient"
+	mUploader "github.com/chainloop-dev/chainloop/internal/casclient/mocks"
+	attestationApi "github.com/chainloop-dev/chainloop/pkg/attestation/crafter/api/attestation/v1"
+	"github.com/chainloop-dev/chainloop/pkg/attestation/crafter/materials"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewGHASCodeScanCrafter(t *testing.T) {
+	testCases := []struct {
+		name    string
+		input   *contractAPI.CraftingSchema_Material
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			input: &contractAPI.CraftingSchema_Material{
+				Type: contractAPI.CraftingSchema_Material_GHAS_CODE_SCAN,
+			},
+		},
+		{
+			name: "wrong type",
+			input: &contractAPI.CraftingSchema_Material{
+				Type: contractAPI.CraftingSchema_Material_SBOM_SPDX_JSON,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := materials.NewGHASCodeScanCrafter(tc.input, nil, nil)
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+		})
+	}
+}
+
+func TestGHASCodeScanCraft(t *testing.T) {
+	testCases := []struct {
+		name     string
+		filePath string
+		wantErr  string
+	}{
+		{
+			name:     "invalid format",
+			filePath: "./testdata/sbom.cyclonedx.json",
+			wantErr:  "unexpected material type",
+		},
+		{
+			name:     "invalid path",
+			filePath: "./testdata/non-existing.json",
+			wantErr:  "no such file or directory",
+		},
+		{
+			name:     "invalid artifact type",
+			filePath: "./testdata/simple.txt",
+			wantErr:  "unexpected material type",
+		},
+		{
+			name:     "missing 'tool' field ß",
+			filePath: "./testdata/ghas-code-scan-wrong.json",
+			wantErr:  "field not found in GHAS code scan",
+		},
+		{
+			name:     "valid artifact type",
+			filePath: "./testdata/ghas-code-scan.json",
+		},
+	}
+
+	assert := assert.New(t)
+	schema := &contractAPI.CraftingSchema_Material{
+		Name: "test",
+		Type: contractAPI.CraftingSchema_Material_GHAS_CODE_SCAN,
+	}
+	l := zerolog.Nop()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Mock uploader
+			uploader := mUploader.NewUploader(t)
+			if tc.wantErr == "" {
+				uploader.On("UploadFile", context.TODO(), tc.filePath).
+					Return(&casclient.UpDownStatus{
+						Digest:   "deadbeef",
+						Filename: "ghas-code-scan.json",
+					}, nil)
+			}
+
+			backend := &casclient.CASBackend{Uploader: uploader}
+			crafter, err := materials.NewGHASCodeScanCrafter(schema, backend, &l)
+			require.NoError(t, err)
+
+			got, err := crafter.Craft(context.TODO(), tc.filePath)
+			if tc.wantErr != "" {
+				assert.ErrorContains(err, tc.wantErr)
+				return
+			}
+			require.NoError(t, err)
+
+			assert.Equal(contractAPI.CraftingSchema_Material_GHAS_CODE_SCAN.String(), got.MaterialType.String())
+			assert.True(got.UploadedToCas)
+
+			// // The result includes the digest reference
+			assert.Equal(got.GetArtifact(), &attestationApi.Attestation_Material_Artifact{
+				Id: "test", Digest: "sha256:fe88fdaf51a4f33029488ade0c2d93620d5676c51833102e139fbe1fbc7694db", Name: "ghas-code-scan.json",
+			})
+		})
+	}
+}
