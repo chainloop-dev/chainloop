4f9b77750507eceb31a5ed2ef1908f336e6585e1
Miguel Martinez Trivino <miguel@chainloop.dev>
2024-11-19 11:42:14 +0100

    feat: add backoff retry to all attestation commands (#1537)
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/cli/cmd/errors.go
+++ app/cli/cmd/errors.go
@@ -17,6 +17,12 @@ package cmd
 
 import (
 	"errors"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	v1 "github.com/chainloop-dev/chainloop/app/controlplane/api/controlplane/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // GracefulError represents an error that has been marked as gracefully handled
@@ -42,3 +48,45 @@ var ErrAttestationNotInitialized = errors.New("attestation not yet initialized, 
 var ErrAttestationAlreadyExist = errors.New("attestation already initialized, to override it use the --replace flag`")
 var ErrAttestationTokenRequired = errors.New("chainloop Token required, please provide it via --token flag or CHAINLOOP_TOKEN environment variable")
 var ErrKeylessNotSupported = errors.New("keyless signing not supported, please provide a private key reference with --key instead")
+
+func isRetriableAPIError(err error) bool {
+	// we retry state conflicts and other transient errors
+	if v1.IsAttestationStateErrorConflict(err) {
+		return true
+	}
+
+	st, ok := status.FromError(err)
+	if !ok {
+		return false
+	}
+
+	retriableCodes := []codes.Code{
+		codes.Unavailable,
+		codes.Internal,
+		codes.ResourceExhausted,
+		codes.DeadlineExceeded,
+	}
+
+	for _, code := range retriableCodes {
+		if st.Code() == code {
+			return true
+		}
+	}
+
+	return false
+}
+
+func runWithBackoffRetry(fn func() error) error {
+	return backoff.RetryNotify(
+		func() error {
+			err := fn()
+			if !isRetriableAPIError(err) {
+				return backoff.Permanent(err)
+			}
+			return err
+		},
+		backoff.NewExponentialBackOff(backoff.WithMaxElapsedTime(3*time.Minute)),
+		func(err error, delay time.Duration) {
+			logger.Err(err).Msgf("retrying in %s", delay)
+		})
+}
