06c5bc51eaf076e93d5b14825902175db0f5411e
Miguel Martinez <miguel@chainloop.dev>
2024-12-05 18:51:49 +0100

    simplify call
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/controlplane/pkg/data/ent/migrate/migrations/20241205150625.sql
+++ app/controlplane/pkg/data/ent/migrate/migrations/20241205150625.sql
@@ -2,8 +2,17 @@
 ALTER TABLE "project_versions" ADD COLUMN "workflow_run_count" bigint NOT NULL DEFAULT 0;
 
 -- Update the "project_versions" table by adding the proper count of workflow runs for each project version
-UPDATE "project_versions" SET "workflow_run_count" = (
-  SELECT COUNT(*)
-  FROM "workflow_runs"
-  WHERE "workflow_runs"."version_id" = "project_versions"."id"
-);
+WITH workflow_run_counts AS (
+  SELECT
+    "version_id" AS id,
+    COUNT(*) AS run_count
+  FROM
+    "workflow_runs"
+  GROUP BY
+    "version_id"
+)
+UPDATE "project_versions"
+SET "workflow_run_count" = workflow_run_counts.run_count
+FROM workflow_run_counts
+WHERE workflow_run_counts.id = "project_versions"."id";
+
