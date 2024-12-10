c1fb982761f2167ff2cda49504876e60e96f7829
Javier Rodriguez <javier@chainloop.dev>
2024-12-10 08:58:00 +0100

    create index without blocking the table
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/controlplane/pkg/data/ent/migrate/migrations/20241210063348.sql
+++ app/controlplane/pkg/data/ent/migrate/migrations/20241210063348.sql
@@ -1,2 +1,2 @@
 -- Create index "workflowrun_version_id_workflow_id" to table: "workflow_runs"
-CREATE INDEX "workflowrun_version_id_workflow_id" ON "workflow_runs" ("version_id", "workflow_id");
+CREATE INDEX CONCURRENTLY "workflowrun_version_id_workflow_id" ON "workflow_runs" ("version_id", "workflow_id");
