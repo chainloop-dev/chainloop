b0b610c71a279da44fa5ada4e5d9796c98d59add
Javier Rodriguez <javier@chainloop.dev>
2024-12-10 09:55:29 +0100

    fix(transaction): Allow atlas to run a concurrent index on the DB
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/controlplane/pkg/data/ent/migrate/migrations/20241210063348.sql
+++ app/controlplane/pkg/data/ent/migrate/migrations/20241210063348.sql
@@ -1,2 +1,4 @@
+-- atlas:txmode none
+
 -- Create index "workflowrun_version_id_workflow_id" to table: "workflow_runs"
 CREATE INDEX CONCURRENTLY "workflowrun_version_id_workflow_id" ON "workflow_runs" ("version_id", "workflow_id");
