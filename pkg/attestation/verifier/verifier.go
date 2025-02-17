352adf6c9bd97c781631d176723680b1829d86f7
Jose I. Paris <jiparis@chainloop.dev>
2025-02-17 17:43:24 +0100

    support malformed but verifiable bundles
    Signed-off-by: Jose I. Paris <jiparis@chainloop.dev>

--- pkg/attestation/verifier/verifier.go
+++ pkg/attestation/verifier/verifier.go
@@ -49,22 +49,46 @@ func VerifyBundle(ctx context.Context, bundleBytes []byte, tr *TrustedRoot) erro
 		return fmt.Errorf("invalid bundle: %w", err)
 	}
 
+	var signingCert *x509.Certificate
 	if bundle.GetVerificationMaterial() == nil || bundle.GetVerificationMaterial().GetCertificate() == nil {
-		// nothing to verify
-		return ErrMissingVerificationMaterial
+		// it's a malformed bundle (according to specs) but still verifiable
+		if bundle.GetVerificationMaterial().GetX509CertificateChain() != nil {
+			certs := bundle.GetVerificationMaterial().GetX509CertificateChain().GetCertificates()
+			if len(certs) == 0 {
+				return ErrMissingVerificationMaterial
+			}
+			rawBytes := certs[0].GetRawBytes()
+			if len(rawBytes) == 0 {
+				return ErrMissingVerificationMaterial
+			}
+
+			var err error
+			signingCert, err = x509.ParseCertificate(rawBytes)
+			if err != nil {
+				return fmt.Errorf("invalid certificate: %w", err)
+			}
+		} else {
+			// nothing to verify
+			return ErrMissingVerificationMaterial
+		}
 	}
 
-	// Use sigstore helpers
-	var sb sigstorebundle.Bundle
-	if err := sb.UnmarshalJSON(bundleBytes); err != nil {
-		return fmt.Errorf("invalid bundle: %w", err)
-	}
+	// Use sigstore helpers to validate and extract materials
+	if signingCert == nil {
+		var sb sigstorebundle.Bundle
+		if err := sb.UnmarshalJSON(bundleBytes); err != nil {
+			return fmt.Errorf("invalid bundle: %w", err)
+		}
 
-	vc, err := sb.VerificationContent()
-	if err != nil {
-		return fmt.Errorf("could not get verification material: %w", err)
+		vc, err := sb.VerificationContent()
+		if err != nil {
+			return fmt.Errorf("could not get verification material: %w", err)
+		}
+		signingCert = vc.GetCertificate()
+		if signingCert == nil {
+			return ErrMissingVerificationMaterial
+		}
 	}
-	signingCert := vc.GetCertificate()
 
 	aki := fmt.Sprintf("%x", sha256.Sum256(signingCert.AuthorityKeyId))
 	chain, ok := tr.Keys[aki]
