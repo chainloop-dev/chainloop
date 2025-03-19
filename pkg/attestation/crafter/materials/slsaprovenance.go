091e5dac1aa3c4f8c50e05034b01dd9d973494ca
Miguel Martinez <miguel@chainloop.dev>
2025-03-19 14:04:34 +0100

    feat: add slsa provenance material type
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

---
+++ pkg/attestation/crafter/materials/slsaprovenance.go
@@ -0,0 +1,71 @@
+//
+// Copyright 2025 The Chainloop Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package materials
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	schemaapi "github.com/chainloop-dev/chainloop/app/controlplane/api/workflowcontract/v1"
+	"github.com/chainloop-dev/chainloop/internal/casclient"
+	api "github.com/chainloop-dev/chainloop/pkg/attestation/crafter/api/attestation/v1"
+	intoto "github.com/in-toto/attestation/go/v1"
+	"github.com/rs/zerolog"
+	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type SLSAProvenanceCrafter struct {
+	*crafterCommon
+	backend *casclient.CASBackend
+}
+
+// SLSA Provenance in the form of Sigstore Bundle
+// https://slsa.dev/spec/v1.0/provenance
+// https://docs.sigstore.dev/about/bundle/
+func NewSLSAProvenanceCrafter(schema *schemaapi.CraftingSchema_Material, backend *casclient.CASBackend, l *zerolog.Logger) (*SLSAProvenanceCrafter, error) {
+	if schema.Type != schemaapi.CraftingSchema_Material_SLSA_PROVENANCE {
+		return nil, fmt.Errorf("material type is not SLSA Provenance")
+	}
+
+	craftCommon := &crafterCommon{logger: l, input: schema}
+	return &SLSAProvenanceCrafter{backend: backend, crafterCommon: craftCommon}, nil
+}
+
+// Craft will calculate the digest of the artifact, simulate an upload and return the material definition
+func (i *SLSAProvenanceCrafter) Craft(ctx context.Context, artifactPath string) (*api.Attestation_Material, error) {
+	data, err := os.ReadFile(artifactPath)
+	if err != nil {
+		return nil, fmt.Errorf("artifact file cannot be read: %w", err)
+	}
+
+	bundle := &protobundle.Bundle{}
+	if err := protojson.Unmarshal(data, bundle); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal bundle: %w", err)
+	}
+
+	statement := &intoto.Statement{}
+	if err := protojson.Unmarshal(bundle.GetDsseEnvelope().GetPayload(), statement); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal statement: %w", err)
+	}
+
+	if p := statement.PredicateType; p != "https://slsa.dev/provenance/v1" {
+		return nil, fmt.Errorf("the provided predicate is not a valid SLSA Provenance: found=%q", p)
+	}
+
+	return uploadAndCraft(ctx, i.input, i.backend, artifactPath, i.logger)
+}
---
+++ pkg/attestation/crafter/materials/slsaprovenance_test.go
@@ -0,0 +1,98 @@
+//
+// Copyright 2025 The Chainloop Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//nolint:dupl
+package materials_test
+
+import (
+	"context"
+	"testing"
+
+	contractAPI "github.com/chainloop-dev/chainloop/app/controlplane/api/workflowcontract/v1"
+	"github.com/chainloop-dev/chainloop/internal/casclient"
+	mUploader "github.com/chainloop-dev/chainloop/internal/casclient/mocks"
+	"github.com/chainloop-dev/chainloop/pkg/attestation/crafter/materials"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInvalidSLSAProvenance(t *testing.T) {
+	assert := assert.New(t)
+	schema := &contractAPI.CraftingSchema_Material{
+		Name: "test",
+		Type: contractAPI.CraftingSchema_Material_SLSA_PROVENANCE,
+	}
+
+	l := zerolog.Nop()
+
+	// Mock uploader
+	uploader := mUploader.NewUploader(t)
+	backend := &casclient.CASBackend{Uploader: uploader}
+	crafter, _ := materials.NewSLSAProvenanceCrafter(schema, backend, &l)
+
+	t.Run("wrong format", func(_ *testing.T) {
+		// Invalid payload
+		_, err := crafter.Craft(context.TODO(), "./testdata/simple.txt")
+		assert.Error(err)
+	})
+
+	t.Run("is not a sigstore bundle but a DSSE envelope", func(_ *testing.T) {
+		// Invalid payload
+		_, err := crafter.Craft(context.TODO(), "./testdata/attestation.json")
+		assert.Contains(err.Error(), "failed to unmarshal bundle")
+	})
+
+	t.Run("it is a bundle but not a valid SLSA Provenance", func(_ *testing.T) {
+		// Invalid payload
+		_, err := crafter.Craft(context.TODO(), "./testdata/chainloop_attestation.sigstore.json")
+		assert.Contains(err.Error(), "the provided predicate is not a valid SLSA Provenance")
+	})
+}
+
+func TestSLSAProvenanceCraft(t *testing.T) {
+	assert := assert.New(t)
+	schema := &contractAPI.CraftingSchema_Material{
+		Name: "test",
+		Type: contractAPI.CraftingSchema_Material_SLSA_PROVENANCE,
+	}
+
+	l := zerolog.Nop()
+
+	// Mock uploader
+	uploader := mUploader.NewUploader(t)
+	uploader.On("UploadFile", context.TODO(), mock.Anything).
+		Return(&casclient.UpDownStatus{
+			Digest:   "deadbeef",
+			Filename: "attestation.json",
+		}, nil)
+
+	backend := &casclient.CASBackend{Uploader: uploader}
+
+	crafter, err := materials.NewSLSAProvenanceCrafter(schema, backend, &l)
+	require.NoError(t, err)
+
+	got, err := crafter.Craft(context.TODO(), "./testdata/slsa_provenance.sigstore.json")
+	assert.NoError(err)
+	assert.Equal(contractAPI.CraftingSchema_Material_SLSA_PROVENANCE.String(), got.MaterialType.String())
+	assert.True(got.UploadedToCas)
+
+	// The result includes the digest reference
+	assert.Equal("test", got.GetArtifact().Id)
+	assert.Equal("sha256:38dc33ded482dcb3efccccea977e24e11bd063bccf499da1cc46487255ff6857", got.GetArtifact().Digest)
+
+	uploader.AssertExpectations(t)
+}
