a5d5988d6b2cf7d5bec12fed3234a48eee72280d
Javier Rodriguez <javier@chainloop.dev>
2025-03-25 01:17:41 +0100

    replace OR for AND
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/controlplane/pkg/data/workflow.go
+++ app/controlplane/pkg/data/workflow.go
@@ -237,7 +237,7 @@ func applyWorkflowFilters(wfQuery *ent.WorkflowQuery, opts *biz.WorkflowListOpts
 					predicates = append(predicates, jsonPredicate)
 				}
 				// Combine the predicates using OR logic
-				selector.Where(sql.Or(predicates...))
+				selector.Where(sql.And(predicates...))
 			})
 		}
 
