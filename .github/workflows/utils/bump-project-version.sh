432a45115e7035969461e7d81f45cc637a0ce024
Javier Rodriguez <javier@chainloop.dev>
2025-04-02 11:22:34 +0200

    feat(ci): Modify version bumper script to allow release candidates
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- .github/workflows/utils/bump-project-version.sh
+++ .github/workflows/utils/bump-project-version.sh
@@ -21,8 +21,24 @@ if [[ -n "${1}" ]]; then
    project_yaml="${1}"
 fi
 
-# load the previous version and BUMP THE MINOR
-version=$(cat ${project_yaml} | awk -F'[ .]' '/^projectVersion:/ {print $2"."$3+1"."0}')
+# Load the previous version and bump appropriately
+version=$(cat "${project_yaml}" | awk '/^projectVersion:/ {
+    version = $2;
+    if (version ~ /-rc/) {
+        # Handle release candidate versions (e.g., v1.0.0-rc.1 -> v1.0.0-rc.2)
+        split(version, parts, /-rc\./);
+        rc_num = parts[2] + 1;
+        print parts[1] "-rc." rc_num;
+    }
+    else {
+        # Load the previous version and BUMP THE MINOR
+        # Handle minor version bumps (e.g., v1.0.0 -> v1.1.0)
+        split(version, ver_parts, /\./);
+        ver_parts[2] = ver_parts[2] + 1;
+        print ver_parts[1] "." ver_parts[2] "." ver_parts[3];
+    }
+}')
 
-## Changes in .chainloop.yml
+# Update the project yaml file
 sed -i "s#^projectVersion:.*#projectVersion: ${version}#g" "${project_yaml}"
+
