3acc341a2ed975023a7ef7397c5867f5d49f5a63
Rafał Kuć <r.kuc@solr.pl>
2025-04-28 20:29:56 +0200

    Revert "Revert "Add support to organization repos with GitHub OIDC token""
    This reverts commit e2db26b13c55790021ccd02280641b5d78cba22a.
    
    Signed-off-by: Rafał Kuć <r.kuc@solr.pl>

--- pkg/attestation/crafter/runners/oidc/github.go
+++ pkg/attestation/crafter/runners/oidc/github.go
@@ -50,7 +50,7 @@ type GitHubOIDCClient struct {
 	requestURL   *url.URL
 	verifierFunc func(context.Context) (*oidc.IDTokenVerifier, error)
 	bearerToken  string
-	actor        string
+	actor        []string
 	audience     []string
 	token        *Token
 }
@@ -79,10 +79,10 @@ func WithAudience(audience []string) Option {
 	}
 }
 
-// WithAudience sets the audience for the OIDC token.
+// WithActor sets the audience for the OIDC token.
 func WithActor(actor string) Option {
 	return func(c *GitHubOIDCClient) {
-		c.actor = actor
+		c.actor = append(c.actor, actor)
 	}
 }
 
@@ -113,6 +113,7 @@ func NewGitHubClient(opts ...Option) (*GitHubOIDCClient, error) {
 	c = GitHubOIDCClient{
 		requestURL:  parsedURL,
 		bearerToken: bearerToken,
+		actor:       []string{},
 	}
 	for _, opt := range opts {
 		opt(&c)
@@ -220,7 +221,7 @@ func (c *GitHubOIDCClient) decodePayload(b []byte) (string, error) {
 }
 
 // verifyToken verifies the token contents and signature.
-func (c *GitHubOIDCClient) verifyToken(ctx context.Context, audience []string, actor string, rawIDToken string) (*oidc.IDToken, error) {
+func (c *GitHubOIDCClient) verifyToken(ctx context.Context, audience []string, actor []string, rawIDToken string) (*oidc.IDToken, error) {
 	// Verify the token.
 	verifier, err := c.verifierFunc(ctx)
 	if err != nil {
@@ -232,9 +233,20 @@ func (c *GitHubOIDCClient) verifyToken(ctx context.Context, audience []string, a
 		return nil, fmt.Errorf("verify: could not verify token: %w", err)
 	}
 
-	// Verify the audience received is the one we requested or is equal to the actor
-	if slices.Compare(audience, idToken.Audience) != 0 && slices.Compare([]string{actor}, idToken.Audience) != 0 {
-		return nil, fmt.Errorf("%w: audience not equal %q != %q or not equal to actor %q", errVerify, audience, idToken.Audience, []string{actor})
+	// Verify the audience received is the one we requested or is equal to any of the actors
+	if slices.Compare(audience, idToken.Audience) != 0 {
+		// Check if any of the actors matches the idToken.Audience
+		actorMatch := false
+		for _, a := range actor {
+			if slices.Compare([]string{a}, idToken.Audience) == 0 {
+				actorMatch = true
+				break
+			}
+		}
+
+		if !actorMatch {
+			return nil, fmt.Errorf("%w: audience not equal %q != %q and none of the actors %q match the audience", errVerify, audience, idToken.Audience, actor)
+		}
 	}
 
 	return idToken, nil
