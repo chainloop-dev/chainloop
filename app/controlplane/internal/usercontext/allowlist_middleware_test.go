ef45b4f53f35a50108711c11a982a207397044e9
Miguel Martinez <miguel@chainloop.dev>
2025-05-15 15:19:30 +0200

    feat: dynamic allow-list
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/controlplane/internal/usercontext/allowlist_middleware_test.go
+++ app/controlplane/internal/usercontext/allowlist_middleware_test.go
@@ -114,7 +114,6 @@ func TestCheckUserInAllowList(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			allowList := &conf.Auth_AllowList{
 				CustomMessage:  tc.customErrMsg,
