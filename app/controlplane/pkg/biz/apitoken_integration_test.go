e720034917357938a33b6fb100089921b59e5be0
Miguel Martinez <miguel@chainloop.dev>
2025-06-26 21:40:41 +0200

    chore: generate token
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/controlplane/pkg/biz/apitoken_integration_test.go
+++ app/controlplane/pkg/biz/apitoken_integration_test.go
@@ -66,7 +66,7 @@ func (s *apiTokenTestSuite) TestCreate() {
 	})
 
 	s.Run("happy path with project", func() {
-		token, err := s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProjectID(s.p1.ID))
+		token, err := s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProject(s.p1))
 		s.NoError(err)
 		s.Equal(s.org.ID, token.OrganizationID.String())
 		s.Equal(s.p1.ID, *token.ProjectID)
@@ -126,7 +126,7 @@ func (s *apiTokenTestSuite) TestCreate() {
 			s.Run(tc.name, func() {
 				var opts []biz.APITokenUseCaseOpt
 				if tc.projectID != nil {
-					opts = append(opts, biz.APITokenWithProjectID(*tc.projectID))
+					opts = append(opts, biz.APITokenWithProject(s.p1))
 				}
 
 				token, err := s.APIToken.Create(ctx, tc.tokenName, nil, nil, s.org.ID, opts...)
@@ -273,7 +273,7 @@ func (s *apiTokenTestSuite) TestList() {
 
 	s.Run("can return only for a specific project", func() {
 		var err error
-		tokens, err := s.APIToken.List(ctx, s.org.ID, false, biz.APITokenWithProjectID(s.p1.ID))
+		tokens, err := s.APIToken.List(ctx, s.org.ID, false, biz.APITokenWithProject(s.p1))
 		s.NoError(err)
 		require.Len(s.T(), tokens, 2)
 		s.Equal(s.t4.ID, tokens[0].ID)
@@ -369,8 +369,8 @@ func (s *apiTokenTestSuite) SetupTest() {
 	require.NoError(s.T(), err)
 
 	// Create 2 tokens for project 1
-	s.t4, err = s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProjectID(s.p1.ID))
+	s.t4, err = s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProject(s.p1))
 	require.NoError(s.T(), err)
-	s.t5, err = s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProjectID(s.p1.ID))
+	s.t5, err = s.APIToken.Create(ctx, randomName(), nil, nil, s.org.ID, biz.APITokenWithProject(s.p1))
 	require.NoError(s.T(), err)
 }
