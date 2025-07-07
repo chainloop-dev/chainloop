33d6d0abd364d54907dad5bb6d50964f8b65bbfc
Javier Rodriguez <javier@chainloop.dev>
2025-07-07 13:57:02 +0200

    fix(group): Allow organization members to get group details
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/controlplane/pkg/authz/authz.go
+++ app/controlplane/pkg/authz/authz.go
@@ -368,6 +368,7 @@ var ServerOperationsMap = map[string][]*Policy{
 	"/controlplane.v1.OrganizationService/ListMemberships": {PolicyOrganizationListMemberships},
 	// Groups
 	"/controlplane.v1.GroupService/List": {PolicyGroupList},
+	"/controlplane.v1.GroupService/Get":  {PolicyGroupRead},
 	// Group Memberships
 	"/controlplane.v1.GroupService/ListMembers": {PolicyGroupListMemberships},
 	// For the following endpoints, we rely on the service layer to check the permissions
