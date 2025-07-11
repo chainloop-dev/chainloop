675c295d172e012087241cb51185624c2fd1b317
Javier Rodriguez <javier@chainloop.dev>
2025-07-11 09:06:39 +0200

    make linter happy
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/controlplane/pkg/data/membership.go
+++ app/controlplane/pkg/data/membership.go
@@ -263,8 +263,7 @@ func (r *MembershipRepo) Delete(ctx context.Context, id uuid.UUID) error {
 
 		// If this is an organization membership and the member type is a user,
 		// we also need to clean up any resource memberships for this user in the organization
-		if membershipToDelete.ResourceType == authz.ResourceTypeOrganization &&
-			membershipToDelete.MembershipType == authz.MembershipTypeUser {
+		if membershipToDelete.ResourceType == authz.ResourceTypeOrganization && membershipToDelete.MembershipType == authz.MembershipTypeUser {
 
 			// Extract the organization id from the membership
 			orgID := membershipToDelete.Edges.Organization.ID
