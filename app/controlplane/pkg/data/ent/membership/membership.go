e8fe8c27f718bd1aeda98519f994d33787648e4f
Miguel Martinez <miguel@chainloop.dev>
2025-07-28 00:01:00 +0100

    add value for ent
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/controlplane/pkg/data/ent/membership/membership.go
+++ app/controlplane/pkg/data/ent/membership/membership.go
@@ -119,7 +119,7 @@ var (
 // RoleValidator is a validator for the "role" field enum values. It is called by the builders before save.
 func RoleValidator(r authz.Role) error {
 	switch r {
-	case "role:org:owner", "role:org:admin", "role:org:viewer", "role:org:member", "role:org:contributor", "role:project:admin", "role:project:viewer", "role:group:maintainer", "role:product:admin", "role:product:viewer":
+	case "role:instance:admin", "role:org:owner", "role:org:admin", "role:org:viewer", "role:org:member", "role:org:contributor", "role:project:admin", "role:project:viewer", "role:group:maintainer", "role:product:admin", "role:product:viewer":
 		return nil
 	default:
 		return fmt.Errorf("membership: invalid enum value for role field: %q", r)
