4a2c5f096d7653a7f42706136a5b10c32c497a9b
Jose I. Paris <jiparis@chainloop.dev>
2025-09-08 19:08:21 +0200

    add concurrent update to index
    Signed-off-by: Jose I. Paris <jiparis@chainloop.dev>

--- app/controlplane/pkg/data/ent/migrate/migrations/20250908160222.sql
+++ app/controlplane/pkg/data/ent/migrate/migrations/20250908160222.sql
@@ -1,4 +1,4 @@
 -- Drop index "workflowrun_state_created_at" from table: "workflow_runs"
 DROP INDEX "workflowrun_state_created_at";
 -- Create index "workflowrun_state_created_at" to table: "workflow_runs"
-CREATE INDEX "workflowrun_state_created_at" ON "workflow_runs" ("state", "created_at");
+CREATE INDEX CONCURRENTLY "workflowrun_state_created_at" ON "workflow_runs" ("state", "created_at");
