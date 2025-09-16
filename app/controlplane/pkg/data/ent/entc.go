4ecf7a26c7202295248248b58a707d8941626b02
Miguel Martinez <miguel@chainloop.dev>
2025-09-16 13:34:50 +0200

    chore: remove entviz
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- app/controlplane/pkg/data/ent/entc.go
+++ app/controlplane/pkg/data/ent/entc.go
@@ -21,8 +21,6 @@ package main
 import (
 	"log"
 
-	"github.com/hedwigz/entviz"
-
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 )
@@ -30,7 +28,7 @@ import (
 func main() {
 	err := entc.Generate("./schema", &gen.Config{
 		Features: []gen.Feature{gen.FeatureVersionedMigration, gen.FeatureModifier, gen.FeatureLock, gen.FeatureUpsert},
-	}, entc.Extensions(entviz.Extension{}))
+	})
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
