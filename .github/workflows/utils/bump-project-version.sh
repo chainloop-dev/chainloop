9938b89dac2cd0bec72d8d7bf768a11d1f3fe2b6
Miguel Martinez <miguel@chainloop.dev>
2025-11-13 14:09:28 +0100

    chore: update chainloop.yaml method
    Signed-off-by: Miguel Martinez <miguel@chainloop.dev>

--- .github/workflows/utils/bump-project-version.sh
+++ .github/workflows/utils/bump-project-version.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Bump the Chainloop project version to the next minor version to the version defined .chainloop.yml
+# Update the Chainloop project version in .chainloop.yml to the provided version + "+next"
 
 set -e
 
 die () {
    echo >&2 "$@"
-   echo "usage: bump-project-version.sh [configFile]"
+   echo "usage: bump-project-version.sh <version> [configFile]"
    exit 1
 }
 
@@ -15,35 +15,18 @@ if [[ -n "${DEBUG}" ]]; then
    set -x
 fi
 
+[ "$#" -ge 1 ] || die "Version argument is required"
+
+version="${1}"
 project_yaml=".chainloop.yml"
 # manual override
-if [[ -n "${1}" ]]; then
-   project_yaml="${1}"
+if [[ -n "${2}" ]]; then
+   project_yaml="${2}"
 fi
 
-if [[ -n "$GITHUB_REF_NAME" ]]; then
-  # Bump from provided version tag
-  version=$(echo $GITHUB_REF_NAME | awk -F'[ .]' '{print $1"."$2+1"."0}')
-else
-  # Load the previous version and bump appropriately
-  version=$(cat "${project_yaml}" | awk '/^projectVersion:/ {
-      version = $2;
-      if (version ~ /-rc/) {
-          # Handle release candidate versions (e.g., v1.0.0-rc.1 -> v1.0.0-rc.2)
-          split(version, parts, /-rc\./);
-          rc_num = parts[2] + 1;
-          print parts[1] "-rc." rc_num;
-      }
-      else {
-          # Load the previous version and BUMP THE MINOR
-          # Handle minor version bumps (e.g., v1.0.0 -> v1.1.0)
-          split(version, ver_parts, /\./);
-          ver_parts[2] = ver_parts[2] + 1;
-          print ver_parts[1] "." ver_parts[2] ".0";
-      }
-  }')
-fi
+# Append "+next" to the version
+version_with_next="${version}+next"
 
 # Update the project yaml file
-sed -i "s#^projectVersion:.*#projectVersion: ${version}#g" "${project_yaml}"
+sed -i "s#^projectVersion:.*#projectVersion: ${version_with_next}#g" "${project_yaml}"
 
