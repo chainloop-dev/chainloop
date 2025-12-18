b38f649e41ede18e2f056bae033af39ac83664a2
Sylwester Piskozub <sylwesterpiskozub@gmail.com>
2025-12-18 19:28:10 +0100

    fmt
    Signed-off-by: Sylwester Piskozub <sylwesterpiskozub@gmail.com>

--- app/cli/cmd/root.go
+++ app/cli/cmd/root.go
@@ -153,7 +153,7 @@ func NewRootCmd(l zerolog.Logger) *cobra.Command {
 
 			// Warn users when the session is interactive, and the operation is supposed to use an API token instead
 			if shouldAskForConfirmation(cmd) && isUserToken && !flagYes {
-logger.Warn().Msg("User-attended mode detected. This is intended for local testing only. For CI/CD or automated workflows, please use an API token.")
+				logger.Warn().Msg("User-attended mode detected. This is intended for local testing only. For CI/CD or automated workflows, please use an API token.")
 				if !confirmationPrompt(fmt.Sprintf("This command will run against the organization %q", orgName)) {
 					return errors.New("command canceled by user")
 				}
