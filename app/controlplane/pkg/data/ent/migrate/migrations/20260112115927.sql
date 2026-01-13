50253b9ee338d0e86417bbd21e442f9c15908dbf
Sylwester Piskozub <sylwesterpiskozub@gmail.com>
2026-01-13 14:44:13 +0100

    migration fix
    Signed-off-by: Sylwester Piskozub <sylwesterpiskozub@gmail.com>

--- app/controlplane/pkg/data/ent/migrate/migrations/20260112115927.sql
+++ app/controlplane/pkg/data/ent/migrate/migrations/20260112115927.sql
@@ -1,2 +1,6 @@
 -- Make organization_id nullable to support instance-level API tokens
 ALTER TABLE "api_tokens" ALTER COLUMN "organization_id" DROP NOT NULL;
+
+-- Create index "apitoken_name" to table: "api_tokens"
+CREATE UNIQUE INDEX "apitoken_name" ON "api_tokens" ("name") WHERE ((revoked_at IS NULL) AND (organization_id IS NULL));
+
