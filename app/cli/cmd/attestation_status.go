7056d63840c89fabb8eaa135f8c41a956d2d5183
Javier Rodriguez <javier@chainloop.dev>
2026-01-15 09:56:33 +0100

    change method signature comment
    Signed-off-by: Javier Rodriguez <javier@chainloop.dev>

--- app/cli/cmd/attestation_status.go
+++ app/cli/cmd/attestation_status.go
@@ -304,7 +304,7 @@ func versionStringAttFinal(p *action.ProjectVersion) string {
 	return p.Version
 }
 
-// removeAnsiCharactersFromBytes removes ANSI escape codes from strings
+// removeAnsiCharactersFromBytes removes ANSI escape codes from bytes slices.
 // Credits to: https://github.com/acarl005/stripansi
 func removeAnsiCharactersFromBytes(input []byte) []byte {
 	const ansiPattern = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
