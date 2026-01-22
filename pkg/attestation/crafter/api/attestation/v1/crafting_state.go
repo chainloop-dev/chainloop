c51dafa422b00e4bdff3779f42d6fc601cd822b0
Sylwester Piskozub <sylwesterpiskozub@gmail.com>
2026-01-22 15:43:55 +0100

    lint
    Signed-off-by: Sylwester Piskozub <sylwesterpiskozub@gmail.com>

--- pkg/attestation/crafter/api/attestation/v1/crafting_state.go
+++ pkg/attestation/crafter/api/attestation/v1/crafting_state.go
@@ -96,9 +96,14 @@ func (m *Attestation_Material) GetEvaluableContent(value string) ([]byte, error)
 	if artifact != nil {
 		if m.InlineCas && m.MaterialType != v1.CraftingSchema_Material_HELM_CHART {
 			rawMaterial = artifact.GetContent()
-		} else if value == "" {
+		}
+
+		if !m.InlineCas && value == "" {
 			return nil, errors.New("artifact path required")
-		} else if m.MaterialType != v1.CraftingSchema_Material_HELM_CHART &&
+		}
+
+		if !m.InlineCas &&
+			m.MaterialType != v1.CraftingSchema_Material_HELM_CHART &&
 			m.MaterialType != v1.CraftingSchema_Material_JUNIT_XML {
 			// read content from local filesystem (except for tgz charts)
 			rawMaterial, err = os.ReadFile(value)
