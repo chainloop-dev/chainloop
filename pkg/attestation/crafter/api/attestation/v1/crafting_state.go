536bb600465dc1b5e1105bcfe783bc5631da6fde
Sylwester Piskozub <sylwesterpiskozub@gmail.com>
2026-01-22 15:07:37 +0100

    fix(policies): prevent binary content parsing for HELM_CHART materials
    Signed-off-by: Sylwester Piskozub <sylwesterpiskozub@gmail.com>

--- pkg/attestation/crafter/api/attestation/v1/crafting_state.go
+++ pkg/attestation/crafter/api/attestation/v1/crafting_state.go
@@ -94,7 +94,9 @@ func (m *Attestation_Material) GetEvaluableContent(value string) ([]byte, error)
 	}
 
 	if artifact != nil {
-		if m.InlineCas {
+		if m.InlineCas &&
+			m.MaterialType != v1.CraftingSchema_Material_HELM_CHART &&
+			m.MaterialType != v1.CraftingSchema_Material_JUNIT_XML {
 			rawMaterial = artifact.GetContent()
 		} else if value == "" {
 			return nil, errors.New("artifact path required")
