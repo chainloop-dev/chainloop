3f3332d16e511a7ba37ccb8c8ffafdb946f8e336
Sylwester Piskozub <sylwesterpiskozub@gmail.com>
2026-01-30 10:48:24 +0100

    lint
    Signed-off-by: Sylwester Piskozub <sylwesterpiskozub@gmail.com>

--- app/controlplane/api/gen/frontend/google/protobuf/wrappers.ts
+++ app/controlplane/api/gen/frontend/google/protobuf/wrappers.ts
@@ -8,6 +8,9 @@ export const protobufPackage = "google.protobuf";
  * Wrapper message for `double`.
  *
  * The JSON representation for `DoubleValue` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface DoubleValue {
   /** The double value. */
@@ -18,6 +21,9 @@ export interface DoubleValue {
  * Wrapper message for `float`.
  *
  * The JSON representation for `FloatValue` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface FloatValue {
   /** The float value. */
@@ -28,6 +34,9 @@ export interface FloatValue {
  * Wrapper message for `int64`.
  *
  * The JSON representation for `Int64Value` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface Int64Value {
   /** The int64 value. */
@@ -38,6 +47,9 @@ export interface Int64Value {
  * Wrapper message for `uint64`.
  *
  * The JSON representation for `UInt64Value` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface UInt64Value {
   /** The uint64 value. */
@@ -48,6 +60,9 @@ export interface UInt64Value {
  * Wrapper message for `int32`.
  *
  * The JSON representation for `Int32Value` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface Int32Value {
   /** The int32 value. */
@@ -58,6 +73,9 @@ export interface Int32Value {
  * Wrapper message for `uint32`.
  *
  * The JSON representation for `UInt32Value` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface UInt32Value {
   /** The uint32 value. */
@@ -68,6 +86,9 @@ export interface UInt32Value {
  * Wrapper message for `bool`.
  *
  * The JSON representation for `BoolValue` is JSON `true` and `false`.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface BoolValue {
   /** The bool value. */
@@ -78,6 +99,9 @@ export interface BoolValue {
  * Wrapper message for `string`.
  *
  * The JSON representation for `StringValue` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface StringValue {
   /** The string value. */
@@ -88,6 +112,9 @@ export interface StringValue {
  * Wrapper message for `bytes`.
  *
  * The JSON representation for `BytesValue` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  */
 export interface BytesValue {
   /** The bytes value. */
